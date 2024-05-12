d0e02eaaf18b8c1b7604f96a44426d994a37b1d5
antoweb <antoweb@gmail.com>
2024-05-12 11:30:58 +0200

    First release hass_backup_s3

---
+++ hass-sync-s3/run.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/with-contenv bashio
+
+CONFIG_PATH=/data/options.json
+
+#####################
+## USER PARAMETERS ##
+#####################
+
+# REQUIRED
+BUCKET_NAME="s3://$(bashio::config 'bucketName')"
+export ENDPOINT_URL="$(bashio::config 'endpointUrl')"
+export REGION="$(bashio::config 'region')"
+export AWS_ACCESS_KEY_ID="$(bashio::config 'accessKey')"
+export AWS_SECRET_ACCESS_KEY="$(bashio::config 'secretKey')"
+export SOURCE_DIR="$(bashio::config 'sourceDir')"
+RESTORE="$(bashio::config 'restore')"
+
+
+###########
+## MAIN  ##
+###########
+
+
+############################
+## SET AWS CLI COMMAND  ##
+############################
+
+echo "Aws cli version: $(aws --version)"
+
+if [[ ${RESTORE} == "true" ]]; then
+    echo "Restoring backups from ${BUCKET_NAME}"
+    duplicity \
+    "${NO_ENCRYPTION}" \
+    --file-prefix-manifest manifest- \
+    --s3-endpoint-url "${ENDPOINT_URL}" \
+    --s3-region-name "${REGION}" \
+    --force \
+    restore \
+    "${BUCKET_NAME}" \
+    "${SOURCE_DIR}"
+else
+    echo "Backuping $(ls -l /${SOURCE_DIR}) to ${BUCKET_NAME}"
+
+    aws s3 sync --endpoint-url "${ENDPOINT_URL}" --region "${REGION}" "${SOURCE_DIR}" "${BUCKET_NAME}"
+
+fi
